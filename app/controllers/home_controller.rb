350af8011d7e906e99e19e4da641e8110cf43b6e
suresh subramani <suresh@luxire.com>
2019-01-18 12:59:20 +0530

    First commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     #@orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
-    #@webhooks = ShopifyAPI::Webhook.find(:all)
+    @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
