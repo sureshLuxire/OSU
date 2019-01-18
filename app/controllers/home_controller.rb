d5cbc5d514cc388124fba812d9f8af647f221a5a
suresh subramani <suresh@luxire.com>
2019-01-18 09:52:09 +0530

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
+    @products = ShopifyAPI::Image.find(:all, params: { limit: 10 })
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     #@orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
