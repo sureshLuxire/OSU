b9e6fef6956c7b096fb3d95705dde52d9830d87c
suresh subramani <suresh@luxire.com>
2019-01-18 10:19:04 +0530

    make it better

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "4ec627be9e82f0d3e81c2edccebbb926"
-  config.secret = "4ec627be9e82f0d3e81c2edccebbb926"
+  config.secret = "a1bd3adb9db2c64027405b1dbd8af18a"
   config.scope = "read_products, write_products, read_orders, write_orders" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
