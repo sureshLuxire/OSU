54945685e5eb9a180bf5cfb2b70456c1cc749f79
suresh subramani <suresh@luxire.com>
2019-01-18 14:23:40 +0530

    first commite

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "ORDER STATUS UPDATE"
   config.api_key = "bbb6140c713eb9afce5f9d730cd1a00a"
   config.secret = "d8089c06b6b4f7416084c2d2ca8cd1a2"
   config.scope = "read_products, write_products, read_orders, write_orders" # Consult this page for more scope options:
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'dashboard#index'
   get 'home/index'
 end
