bb987e29f8d70d1cd07379007ebe6d64f06af7bc
suresh subramani <suresh@luxire.com>
2019-01-21 17:26:53 +0530

    better commit

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -5,7 +5,6 @@ ShopifyApp.configure do |config|
   config.scope = "read_products, write_products, read_orders, write_orders" 
   # Consult this page for more scope options:
   # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
-  config.application = true
   config.embedded_app = true  
   config.after_authenticate_job = false
   config.session_repository = Shop
