5ea41692d99efee8dc0fdd806f301fa20e2b23d6
suresh subramani <suresh@luxire.com>
2019-01-28 06:22:45 +0530

    better commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @title = 'Order Status Update'
+    
     @products = ShopifyAPI::Product.find(:all, params: { limit: 100 })
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 100 })
     @customers = ShopifyAPI::Customer.find(:all params: { limit: 100000 })
