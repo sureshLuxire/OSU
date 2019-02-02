89c87eb473e7ff52805be5062d654bc50d6a0e39
suresh subramani <suresh@luxire.com>
2019-02-02 12:10:19 +0530

    better commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
   def index
     #@orders = Order.all
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 250})
-    @lineItems = ShopifyAPI::LineItem.find(:all)
+    @line_items = ShopifyAPI::LineItem.find(:all)
     @products = ShopifyAPI::Product.find(:all)
     #@product = ShopifyAPI::Product.find(params[:id])
   end
