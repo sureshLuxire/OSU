7b157e74b5646611db93c4e8f3555e43f2718613
suresh subramani <suresh@luxire.com>
2019-02-04 00:34:32 +0530

    better commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ShopifyApp::AuthenticatedController
   def index
     @title = 'Order Status Update'
     @products = ShopifyAPI::Product.find(:all)
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 3 })    
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 250 })    
   end 
 end
